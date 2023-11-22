0545c4ef4434ea98c8be0b09c45f275bd72da8d1
Austin Szatrowski <austinszatrowski@gmail.com>
2023-11-22 09:24:22 -0500

    converted np.array to pd.dataframe for efficiency

--- figure1.py
+++ figure1.py
@@ -1,11 +1,12 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt 
 
 # Model dimensional parameters
 dt = 1 # set the ts step
 Tmax = 400 # set the maximum ts
 ts_seq = np.arange(0, Tmax+dt, dt) # sequence of ts values
-depth = 10000 # n iterations of APD per ts value
+depth = 5000 # n iterations of APD per ts value
 
 # Experimental constants
 A = 270
@@ -19,8 +20,8 @@ n = 1
 
 lastn = 50 #include only last n from each run to filter out initial condition-dependent behavior
 
-x = np.zeros((depth+1, len(ts_seq)), dtype=np.float64) #preallocate the solution array. first row is the ts values, and the columns below it are all the solutions for that ts.
-x[0,:] = ts_seq # first row of values is ts
+x = np.zeros((len(ts_seq), depth+1), dtype=np.float64) #preallocate the solution array. first row is the ts values, and the columns below it are all the solutions for that ts.
+x[:,0] = ts_seq # first row of values is ts
 
 
 def eqn1(x_i, ts, A, B1, B2, T1, T2):
@@ -29,14 +30,16 @@ def eqn1(x_i, ts, A, B1, B2, T1, T2):
         result = DImin
     return(result)
 
-for ts in np.arange(len(x[0,:])):
-    for i in np.arange(0, len(x[:,ts])-1):
-        x[i+1,ts] = eqn1(x[i,ts], ts, A, B1, B2, T1, T2)
+for ts in np.arange(len(x[:,0])):
+    for i in np.arange(0, len(x[ts,:])-1):
+        x[ts,i+1] = eqn1(x[ts,i], ts, A, B1, B2, T1, T2)
+
+solution_df = pd.DataFrame(data = x)
+solution_df.rename(columns = {0:'ts'}, inplace = True)
 
 plt.figure(dpi=200)
-for row in np.arange(x.shape[0]-lastn, x.shape[0]):
-    plt.scatter(x[0,:], x[row,:], c = 'b', alpha = 0.01, s=1)
-    #plt.scatter(x[0,:], x[row,:], s=1)
+for col in np.arange(solution_df.shape[0]-lastn, solution_df.shape[0]):
+    plt.scatter(solution_df["ts"], solution_df[col], c = 'b', alpha = 0.01, s=1)
 plt.title("$APD_i$ versus $t_s$")
 plt.xlabel("Initial Stimulation $t_s$ (ms)")
 plt.ylabel("$APD_i$")
